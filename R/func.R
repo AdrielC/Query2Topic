8e5ae795c0006b17f89d4f467bb02567a9e77a40
adrielc <adrielcasellas@gmail.com>
2018-08-14 19:29:25 -0600

    preprocess update

--- R/func.R
+++ R/func.R
@@ -5,14 +5,14 @@ library(parallel)
 library(textstem)
 
 
-disclaimer1 <- "While we aim to supply accurate product information it is sourced by manufacturers suppliers and marketplace sellers and has not been provided by Overstock"
+disclaimer <- "While we aim to supply accurate product information it is sourced by manufacturers suppliers and marketplace sellers and has not been provided by Overstock"
 disclaimer2 <- "While we aim to supply accurate product information, it is sourced by manufacturers, suppliers and marketplace sellers, and has not been provided by Overstock"
 
 # split_text_file(): ------------------------------------------------------
 ### Only works on a Unix system with coreutils installed
 ### If function fails with split function, try removing the g suffix from gsplit.
 
-split_text_file <- function(in.file.path, line.count = 10000, csv = TRUE) {
+split_text_file <- function(in.file.path, line.count = 10000) {
   if(!file.exists(in.file.path)) {
     stop(paste("File:", in.file.path, "could not be found"))
   }
@@ -22,28 +22,24 @@ split_text_file <- function(in.file.path, line.count = 10000, csv = TRUE) {
   fileid <- str_extract(in.file.path, "[ \\w-]+?(?=\\.)")
   
   system(
-    paste(
-      paste("cd", filepath, "&& mkdir", fileid, "&& cd", fileid), "&&",
-      paste0("gsplit -l ", line.count, " --numeric-suffixes ", "../", filename, " ", fileid)))
+    paste0(
+      paste0("perl -e '$/=\"\"; while(<>){ $_ =~ s/\\n(?!\\b\\d*,\\b)/$1 /g; print; }' ", in.file.path, " > ", filepath, "tmp.csv", " && "),
+      paste0("cd ", filepath, " && mkdir ", fileid, " && cd ", fileid), " && ", 
+      "tail -n +2 ../tmp.csv", " | gsplit -l ", line.count, " - split_
+      for file in split_*
+        do
+      head -n 1 ../tmp.csv", " > tmp_file
+      cat $file >> tmp_file
+      mv -f tmp_file $file
+      done"
+    )
+  )
   
-  if(csv == TRUE) {
-    file_dir <- paste0(filepath, fileid)
-    file_list <- list.files(file_dir)
-    first_file <- file_list[[1]]
-    col_names <- paste0(colnames(read_csv(paste0(file_dir, "/", first_file))), collapse = ", ")
-    files_to_append <- file_list[-1]
+  system(paste0("rm ", filepath, "tmp.csv"))
   
-    for(file in seq_along(files_to_append)) {
-      system(
-        paste0("sed  -i '' '1i\\\n",  col_names, "\n' ", paste0(file_dir, "/", files_to_append[file])))
-    }
-  }
-  
+  dir <- paste0(filepath, fileid, "/")
+  return(dir)
 }
-
-# Testing
-# split_text_file(in.file.path = "../data/test/test_query.csv")
-
 
 
 # process_pipeline(): -----------------------------------------------------------
@@ -54,7 +50,6 @@ process_pipe <- function(product_text) {
     mutate(text = map_chr(text, clean_text)) %>% 
     mutate(word_count = str_count(text, "\\S+"))
 }
-
 
 
 # clean_text(): -----------------------------------------------------------
@@ -71,9 +66,6 @@ clean_text <- function(text) {
 }
 
 
-
-
-
 # file_pipe(): ------------------------------------------------------------
 
 file_pipe <- function(file) {
@@ -84,42 +76,55 @@ file_pipe <- function(file) {
 }
 
 
-
-
 # process_split_files(): -----------------------------------------------------
 
 process_split_files <- function(dir, cl = detectCores() - 1) {
   file_list <- list.files(dir)
   file_list <- vapply(file_list, function(x) paste0(dir, x), FUN.VALUE = "character")
   pblapply(file_list, file_pipe, cl = cl)
+  return(dir)
 }
 
 
+# merge_csv(): ------------------------------------------------------------
+
+merge_csv <- function(dir, out_file) {
+  system(
+    paste0(
+      "OutFileName='", out_file, "'
+      i=0
+      for filename in ", dir, "*; do
+        if [ '$filename'  != '$OutFileName' ] ;
+        then
+          if [[ $i -eq 0 ]] ; then
+            head -1  $filename >   $OutFileName
+          fi
+          tail -n +2  $filename >>  $OutFileName
+          i=$(( $i + 1 ))
+        fi
+      done"
+    )
+  )
+}
 
 
 # preprocess_data(): ---------------------------------------------------------
 
-preprocess_data <- function(db_export_file) {
-  
+preprocess_data <- function(db_export_file, out_file, ...) {
+  db_export_file %>% 
+    split_text_file(...) %>% 
+    process_split_files() %>% 
+    merge_csv(out_file)
 }
 
 
 
 
-# testing
-
-test_data_raw <- read_csv("../data/product_text/table_export_DATA.csv", n_max = 10000)
-
-test_out <- process_pipeline(test_data_raw)
-
-test_out[43,2]
-test_out1[43,2]
-
-hist(test_out$word_count, breaks = 100, xlim = c(0,100))
 
 
-split_text_file("../data/test/test_chunk.csv", line.count = 10)
 
+# testing -----------------------------------------------------------------
 
-file_pipe("../data/test/test_chunk copy.csv")
+preprocess_data("../data/test/test_chunk_utf.csv", "../data/test/test_out.csv", line.count = 2)
 
+preprocess_data("../data/test/test_chunk_utf.csv", "../data/test/test_out.csv", line.count = 2)
